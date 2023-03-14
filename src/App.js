2331afd2bef566158f932cf29f3409d913ab1de9
mujeebdev <mujeeb@psychx86.com>
2023-03-14 14:57:35 +0530

    new file has been added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/Navbar/NavBar";
+import SideNavBar from "./components/Sidebar/SideNavBar";
+// import logo from "./assets/navImage.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <SideNavBar />
     </div>
   );
 }
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from "react-bootstrap/Dropdown";
+import logo from "../../assets/navImage.png";
+import Nrm_photo_icon_dummy from "../../assets/Nrm_photo_icon_dummy.png";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import "./navbar.css";
+import { NavDropdown, Image } from "react-bootstrap";
+// import "./App.css";
+import { FaShoppingCart } from "react-icons/fa";
+
+function NavBar() {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            
+            <div className="navbar_image ">
+              <img src={logo} alt="" />
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse
+            id="navbarScroll"
+            className="d-flex justify-content-center align-items-center"
+          >
+            {/* <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+             ></Nav> */}
+            <Form className="d-flex justify-content-center align-items-center">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <div className="Checkout-button">
+                <Button variant="dark">
+                  <FaShoppingCart />
+                  Checkout(200)
+                </Button>
+              </div>
+            </Form>
+          </Navbar.Collapse>
+          <div>
+            <NavDropdown title="User Admin" id="basic-nav-dropdown">
+              {/* <NavDropdown.Item> */}
+              <Image
+                className="thumbnail-image"
+                src={Nrm_photo_icon_dummy}
+                alt=""
+              />
+              {/* </NavDropdown.Item> */}
+
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
